8106bb1eb43738933a174f45b962ed076d4dab39
Kaushalvadadoria28 <kaushalvadadoria28@gmail.com>
2024-09-01 14:26:14 +0530

    Test for adding a new book has been finished

---
+++ tests_for_library.py
@@ -0,0 +1,17 @@
+import unittest
+from library import Library,Book
+
+class TestLibrary(unittest.TestCase):
+
+    def setUp(self):
+        self.library = Library()
+        self.book1 = Book(isbn="1234567890", title="Python Programming", author="Kaushal vadadoria", publication_year=2015)
+        self.book2 = Book(isbn="0987654321", title="Data Science 101", author="Kaushal vadadoria", publication_year=2017)
+
+    def test_add_book(self):
+        self.library.add_book(self.book1)
+        self.assertIn("1234567890", self.library.books)
+        self.assertEqual(self.library.books["1234567890"], self.book1)
+
+if __name__ == "__main__":
+    unittest.main()
